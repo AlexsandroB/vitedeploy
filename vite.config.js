5e4823f63ffa1763c861034b9aca5e4d381fc778
AlexsandroB <71601969+AlexsandroB@users.noreply.github.com>
2022-12-15 20:29:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
 
   resolve:{
     alias: {
-      '@img' : path.resolve(__dirname, './src/img')
+      '@img' : path.resolve(__dirname, './src/img'),
+      '@pages' : path.resolve(__dirname, './src/pages')
     }
   }
 })
